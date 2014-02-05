c0ac35f775c3737700a2a26aaae62c8de74e3de0
Harry Culpan <harry@tesseract.org>
2014-02-05 13:32:17 -0500

    First commit

---
+++ src/main.go
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: harry
+ * Date: 2/5/14
+ * Time: 1:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello world!")
+}
